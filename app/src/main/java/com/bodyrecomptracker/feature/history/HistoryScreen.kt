c3447f22fa994b81dccd830f4f2bcfb932b91086
Backup System <backup@projeto.com>
2026-01-31 23:01:33 -0300

    fix(build): importar Box e setValue na tela de Histrico

--- app/src/main/java/com/bodyrecomptracker/feature/history/HistoryScreen.kt
+++ app/src/main/java/com/bodyrecomptracker/feature/history/HistoryScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.background
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
@@ -22,6 +23,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
